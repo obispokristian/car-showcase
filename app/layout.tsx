0cd08b822f27fe80e5c81fce687c2661a31a804d
Kristian Obispo <105583613+obispokristian@users.noreply.github.com>
2023-08-02 03:37:33 +0800

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import { Footer, Navbar } from "@/components";
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Carhub",
+  description: "Car rental app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
+    </html>
+  );
+}
